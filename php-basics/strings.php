6289505643029facb88152b8d319fb4a371a4e53
Jules Sepulveda <julietrsepulveda@gmail.com>
2018-02-17 15:12:17 -0600

    combining strings

--- php-basics/strings.php
+++ php-basics/strings.php
@@ -1,5 +1,10 @@
 <?php
 $name = 'Jules';
-$string_one = "Learning to display \"Hello $name!\" to the screen! \n";
+$string_one = "Learning to display \"Hello $name!\" to the screen.\n";
+$string_one = 'Learning to display "Hello ';
+$string_one .= $name . '!" to the screen.';
+$string_one =  $string_one . "\n";
+//prepend to a string
+$string_one = 'I am ' . $string_one;
 echo $string_one;
 ?>
