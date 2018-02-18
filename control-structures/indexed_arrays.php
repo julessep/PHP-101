5d33404d3850e4760b8d0f24140a9796a629703b
Jules Sepulveda <julietrsepulveda@gmail.com>
2018-02-18 14:07:00 -0600

    loop intro

--- control-structures/indexed_arrays.php
+++ control-structures/indexed_arrays.php
@@ -2,14 +2,15 @@
 $learn = ['Learn Conditionals', 'Learn Arrays', 'Learn Loops'];
 $learn[] = 'Build something awesome!';
 array_push($learn, 'Functions', 'Forms', 'Objects');
-// array_unshift($learn, 'Functions', 'Forms', 'Objects');
+array_unshift($learn, 'Functions', 'Forms', 'Objects');
+asort($learn);
 $removed = array_shift($learn);
 echo 'you removed ' . array_shift($learn);
 unset($learn[1],$learn[2]);
 $learn = array_values($learn);
 $learn[1] = 'Learn Painting';
 $learn[4] = 'Learn Skiing';
-// var_dump($learn);
+var_dump($learn);
 // echo $learn[1];
 // echo implode("/n", $learn);
 ?>
---
+++ control-structures/loops.php
@@ -0,0 +1,23 @@
+<?php 
+$currentYear = date('Y');
+$year = $currentYear - 100;
+
+// while ($year <= $currentYear) {
+//   echo $year . "<br />\n";
+//   $year++;
+// }
+// do {
+//   echo $year . "<br />\n";
+// } while (++$year <= $currentYear);
+$learn = ['Learn Conditionals', 'Learn Arrays', 'Learn Loops'];
+$learn[] = 'Build something awesome!';
+array_push($learn, 'Functions', 'Forms', 'Objects');
+array_unshift($learn, 'Functions', 'Forms', 'Objects');
+asort($learn);
+
+$count=0;
+while ((list($key, $val) = each($learn)) && (++$count <= 2)) {
+    echo "$key => $val\n";
+}
+
+?>
